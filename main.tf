6693218ccbbadf930d54ac92f33c276c93e61dbe
pavelsoloduha <pavel.soloduha@outlook.com>
2019-10-31 18:52:55 +0300

    add creating subnets and edit default route table

--- main.tf
+++ main.tf
@@ -1,39 +1,74 @@
 # Configure the AWS Provider
 provider "aws" {
   version = "~> 2.0"
-  region  = "us-east-2"
+  region  = var.region
 }
 
 # Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/24"
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr["vpc"]
 
   tags = {
-    Name = "vpc"
-    provider = "terraform"
+    Name     = "vpc"
+    provider = var.tag-provider
   }
 }
 
 # Create Internet Gateway
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
   tags = {
-    Name = "gateway"
-    provider = "terraform"
+    Name     = "gateway"
+    provider = var.tag-provider
   }
 }
 
 # Create subnets
-resource "aws_subnet" "main" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.0.32/27"
-  availability_zone = "us-east-2a"
+resource "aws_subnet" "private-db-a" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = var.cidr["subnet-pr-db-a"]
+  availability_zone = var.AZ["A"]
 
   tags = {
-    Name = "PrivateDB-A"
-    provider = "terraform"
-    AZ = "us-east-2a"
+    Name     = "PrivateDB-A"
+    provider = var.tag-provider
+    AZ       = var.AZ["A"]
+  }
+}
+
+resource "aws_subnet" "private-backend-a" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = var.cidr["subnet-pr-bk-a"]
+  availability_zone = var.AZ["A"]
+
+  tags = {
+    Name     = "PrivateBackend-A"
+    provider = var.tag-provider
+    AZ       = var.AZ["A"]
+  }
+}
+
+resource "aws_subnet" "private-public" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = var.cidr["subnet-pub-a"]
+  availability_zone = var.AZ["A"]
+  
+  tags = {
+    Name     = "Public-A"
+    provider = var.tag-provider
+    AZ       = var.AZ["A"]
+  }
+}
+
+resource "aws_default_route_table" "default-rt" {
+  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+
+  tags = {
+    Name     = "MainRT"
+    provider = var.tag-provider
+    AZ       = var.AZ["A"]
+  
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "tag-provider" {
+  default = "terraform"
+}
+
+variable "cidr" {
+  type = "map"
+  default = {
+    "vpc"            = "10.0.0.0/24"
+    "subnet-pub-a"   = "10.0.0.0/27"
+    "subnet-pr-db-a" = "10.0.0.32/27"
+    "subnet-pr-bk-a" = "10.0.0.64/27"
+  }
+}
+
+variable "AZ" {
+  type = "map"
+  default = {
+    "A" = "us-east-2a"
+    "B" = "us-east-2b"
+  }
+}

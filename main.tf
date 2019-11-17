237b17181d4b0d63afa79204c7f2b1b55d056058
pavelsoloduha <pavel.soloduha@outlook.com>
2019-11-17 21:57:29 +0300

    move security groups to file-module

--- main.tf
+++ main.tf
@@ -14,31 +14,31 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-module "security-groups" {
-  source = "./security-groups"
-
-  vpc_id       = aws_vpc.vpc.id
-  tag_provider = var.tag_provider
+data "aws_vpc" "vpc_data" {
+  id = aws_vpc.vpc.id
 }
 
-module "network" {
-  source = "./network"
-
-  vpc_id                         = aws_vpc.vpc.id
-  tag_provider                   = var.tag_provider
-  aval_zone_count                = 1
-  sec_group_id_allow_inbound     = module.security-groups.sec_group_id_allow_inboud
-  sec_group_id_allow_vpc_traffic = module.security-groups.sec_group_id_allow_vpc_traffic
+data "aws_availability_zones" "aval_zones" {
 }
 
-module "instances" {
-  source = "./instances"
-
-  sec_group_id_allow_inbound     = module.security-groups.sec_group_id_allow_inboud
-  sec_group_id_allow_vpc_traffic = module.security-groups.sec_group_id_allow_vpc_traffic
-  subnet_id_public_a             = module.network.subnet_id_public_a
-  tag_provider                   = var.tag_provider
-  sec_group_id_allow_ssh         = module.security-groups.sec_group_id_allow_ssh
-  subnet_id_private_backend_a    = module.network.subnet_id_private_backend_a
-  subnet_id_private_db_a         = module.network.subnet_id_private_db_a
-}
+//module "network" {
+//  source = "./network"
+//
+//  vpc_id                         = aws_vpc.vpc.id
+//  tag_provider                   = var.tag_provider
+//  aval_zone_count                = 1
+//  sec_group_id_allow_inbound     = module.security-groups.sec_group_id_allow_inboud
+//  sec_group_id_allow_vpc_traffic = module.security-groups.sec_group_id_allow_vpc_traffic
+//}
+//
+//module "instances" {
+//  source = "./instances"
+//
+//  sec_group_id_allow_inbound     = module.security-groups.sec_group_id_allow_inboud
+//  sec_group_id_allow_vpc_traffic = module.security-groups.sec_group_id_allow_vpc_traffic
+//  subnet_id_public_a             = module.network.subnet_id_public_a
+//  tag_provider                   = var.tag_provider
+//  sec_group_id_allow_ssh         = module.security-groups.sec_group_id_allow_ssh
+//  subnet_id_private_backend_a    = module.network.subnet_id_private_backend_a
+//  subnet_id_private_db_a         = module.network.subnet_id_private_db_a
+//}
--- security-groups/input.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  type        = string
-  description = "Id of VPC"
-}
-
-variable "tag_provider" {
-  type        = string
-  description = "Name of infrastructure provider"
-}

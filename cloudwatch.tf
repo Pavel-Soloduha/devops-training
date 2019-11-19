c264a132644235ba0a383c69981f23f1995857ae
Pavel Solodukha <pavelsolodukha@coherentsolutions.com>
2019-11-19 17:31:29 +0300

    change cloudwatch

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_metric_alarm" "cpu_util" {
   alarm_name          = "cpu_util"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  threshold           = "2"
+  evaluation_periods  = "2"
+  threshold           = "1"
   metric_name         = "CPUUtilization"
-  statistic           = "Average"
-  period              = 60
+  statistic           = "Maximum"
+  period              = "60"
   namespace           = "AWS/EC2"
   alarm_actions       = [data.aws_sns_topic.smtp_topic.arn]
   ok_actions          = [data.aws_sns_topic.smtp_topic.arn]

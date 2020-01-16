87cd049142074942d57b7dadd53d57be440ff244
pavelsoloduha <pavel.soloduha@outlook.com>
2020-01-17 00:58:47 +0300

    Added rundeck instance to use

--- private_infra.tf
+++ private_infra.tf
@@ -204,7 +204,7 @@ resource "aws_instance" "awx" {
   tags = merge(
     var.common_tags,
     map(
-      "Name", "AWX- ${element(data.aws_availability_zones.zones.names, count.index)}",
+      "Name", "AWX-${element(data.aws_availability_zones.zones.names, count.index)}",
       "app-type", "awx",
       "AZ", element(data.aws_availability_zones.zones.names, 0)
     )
@@ -257,3 +257,64 @@ resource "aws_lb_target_group_attachment" "awx_target_attachment" {
 
 //rundeck
 //t3a.small
+resource "aws_instance" "rundeck" {
+  count                  = var.infra_subnets_count
+  ami                    = var.default_ami
+  instance_type          = "t3a.small"
+  subnet_id              = aws_subnet.private_infra.*.id[0]
+  key_name               = var.access_key
+  vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
+
+  tags = merge(
+    var.common_tags,
+    map(
+      "Name", "Rundeck-${element(data.aws_availability_zones.zones.names, count.index)}",
+      "app-type", "rundeck",
+      "AZ", element(data.aws_availability_zones.zones.names, 0)
+    )
+  )
+}
+
+resource "aws_route53_record" "rundeck-route53" {
+  zone_id = data.aws_route53_zone.test_zone.id
+  name    = "rundeck-solodukha.test.coherentprojects.net"
+  type    = "CNAME"
+  ttl     = "60"
+  records = [aws_lb.alb.dns_name]
+}
+
+resource "aws_lb_listener_rule" "rundeck_lb_rule" {
+  listener_arn = aws_lb_listener.alb-default-listener.arn
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.rundeck_target.arn
+  }
+  condition {
+    field  = "host-header"
+    values = ["rundeck-solodukha.*"]
+  }
+}
+
+resource "aws_lb_target_group" "rundeck_target" {
+  name     = "solodukha-rundeck-target"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = merge(
+    var.common_tags
+  )
+
+  health_check {
+    enabled = true
+    timeout = 10
+    matcher = "200-499"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "rundeck_target_attachment" {
+  count            = var.infra_subnets_count
+  target_group_arn = aws_lb_target_group.rundeck_target.arn
+  target_id        = element(aws_instance.rundeck.*.id, count.index)
+  port             = 80
+}

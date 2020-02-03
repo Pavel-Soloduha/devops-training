ed952d3339b9c86f7b2d9f63be8cc3a57651b744
Pavel Solodukha <pavelsolodukha@coherentsolutions.com>
2020-02-03 18:48:02 +0300

    Comment unused resources Minimize Nexus instance

--- private_infra.tf
+++ private_infra.tf
@@ -82,58 +82,10 @@ resource "aws_lb_target_group_attachment" "jenkins_target_attachment" {
   port             = 80
 }
 
-//resource "aws_instance" "sonar-qube" {
-//  ami =  var.default_ami
-//  instance_type = "c5.xlarge"
-//  subnet_id = aws_subnet.private_infra.*.id[0]
-//  key_name = var.access_key
-//  vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
-//
-//  tags = merge(
-//  var.common_tags,
-//  map(
-//  "Name", "sonar-qube",
-//  "AZ", element(data.aws_availability_zones.zones.names, 0)
-//  )
-//  )
-//}
-
-//resource "aws_instance" "gitlab" {
-//  ami                    = var.default_ami
-//  instance_type          = "t3a.large"
-//  subnet_id              = aws_subnet.private_infra.*.id[0]
-//  key_name               = var.access_key
-//  vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
-//
-//  tags = merge(
-//    var.common_tags,
-//    map(
-//      "Name", "gitlab",
-//      "AZ", element(data.aws_availability_zones.zones.names, 0)
-//    )
-//  )
-//}
-//
-//resource "aws_instance" "gitlab-runner" {
-//  ami                    = var.default_ami
-//  instance_type          = "m3.medium"
-//  subnet_id              = aws_subnet.private_infra.*.id[0]
-//  key_name               = var.access_key
-//  vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
-//
-//  tags = merge(
-//    var.common_tags,
-//    map(
-//      "Name", "gitlab-runner",
-//      "AZ", element(data.aws_availability_zones.zones.names, 0)
-//    )
-//  )
-//}
-
 resource "aws_instance" "nexus" {
   count                  = var.infra_subnets_count
   ami                    = var.default_ami
-  instance_type          = "c5.xlarge"
+  instance_type          = "a1.xlarge"
   subnet_id              = aws_subnet.private_infra.*.id[0]
   key_name               = var.access_key
   vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
@@ -193,67 +145,67 @@ resource "aws_lb_target_group_attachment" "nexus_target_attachment" {
 }
 
 //awx
-resource "aws_instance" "awx" {
-  count                  = var.infra_subnets_count
-  ami                    = var.default_ami
-  instance_type          = "m3.medium"
-  subnet_id              = aws_subnet.private_infra.*.id[0]
-  key_name               = var.access_key
-  vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
-
-  tags = merge(
-    var.common_tags,
-    map(
-      "Name", "AWX-${element(data.aws_availability_zones.zones.names, count.index)}",
-      "app-type", "awx",
-      "AZ", element(data.aws_availability_zones.zones.names, 0)
-    )
-  )
-}
-
-resource "aws_route53_record" "awx-route53" {
-  zone_id = data.aws_route53_zone.test_zone.id
-  name    = "awx-solodukha.test.coherentprojects.net"
-  type    = "CNAME"
-  ttl     = "60"
-  records = [aws_lb.alb.dns_name]
-}
-
-resource "aws_lb_listener_rule" "awx_lb_rule" {
-  listener_arn = aws_lb_listener.alb-default-listener.arn
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.awx_target.arn
-  }
-  condition {
-    field  = "host-header"
-    values = ["awx-solodukha.*"]
-  }
-}
-
-resource "aws_lb_target_group" "awx_target" {
-  name     = "solodukha-awx-target"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
-
-  tags = merge(
-    var.common_tags
-  )
-
-  health_check {
-    enabled = true
-    timeout = 10
-    matcher = "200-499"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "awx_target_attachment" {
-  count            = var.infra_subnets_count
-  target_group_arn = aws_lb_target_group.awx_target.arn
-  target_id        = element(aws_instance.awx.*.id, count.index)
-  port             = 80
-}
+//resource "aws_instance" "awx" {
+//  count                  = var.infra_subnets_count
+//  ami                    = var.default_ami
+//  instance_type          = "m3.medium"
+//  subnet_id              = aws_subnet.private_infra.*.id[0]
+//  key_name               = var.access_key
+//  vpc_security_group_ids = [aws_security_group.allow-vpc-traffic.id, aws_security_group.allow-inbound.id]
+//
+//  tags = merge(
+//    var.common_tags,
+//    map(
+//      "Name", "AWX-${element(data.aws_availability_zones.zones.names, count.index)}",
+//      "app-type", "awx",
+//      "AZ", element(data.aws_availability_zones.zones.names, 0)
+//    )
+//  )
+//}
+//
+//resource "aws_route53_record" "awx-route53" {
+//  zone_id = data.aws_route53_zone.test_zone.id
+//  name    = "awx-solodukha.test.coherentprojects.net"
+//  type    = "CNAME"
+//  ttl     = "60"
+//  records = [aws_lb.alb.dns_name]
+//}
+//
+//resource "aws_lb_listener_rule" "awx_lb_rule" {
+//  listener_arn = aws_lb_listener.alb-default-listener.arn
+//  action {
+//    type             = "forward"
+//    target_group_arn = aws_lb_target_group.awx_target.arn
+//  }
+//  condition {
+//    field  = "host-header"
+//    values = ["awx-solodukha.*"]
+//  }
+//}
+//
+//resource "aws_lb_target_group" "awx_target" {
+//  name     = "solodukha-awx-target"
+//  port     = 80
+//  protocol = "HTTP"
+//  vpc_id   = aws_vpc.vpc.id
+//
+//  tags = merge(
+//    var.common_tags
+//  )
+//
+//  health_check {
+//    enabled = true
+//    timeout = 10
+//    matcher = "200-499"
+//  }
+//}
+//
+//resource "aws_lb_target_group_attachment" "awx_target_attachment" {
+//  count            = var.infra_subnets_count
+//  target_group_arn = aws_lb_target_group.awx_target.arn
+//  target_id        = element(aws_instance.awx.*.id, count.index)
+//  port             = 80
+//}
 
 //rundeck
 //t3a.small
